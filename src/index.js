69f9fa7db90558d0a07003355da305c3f8e3d80e
DotNetKolz <dotnetkolz@gmail.com>
2021-10-23 13:29:35 -0400

    Using SAM template to setup table and permissions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 'use strict';
 
+module.exports.test = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Test executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+
+};
+
 module.exports.add = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Add executed successfully!',
-      input: event,
+      message: 'Add executed successfully!'
     }),
   };
 
@@ -18,8 +30,7 @@ module.exports.get = (event, context, callback) => {
     const response = {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'Get executed successfully!',
-        input: event,
+        message: 'Get executed successfully!'
       }),
     };
   
